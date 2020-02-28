2d54985c32e4e058ad0a5b07b9cca0a3c3244dbc
shumkova <a.shumkova@live.com>
2020-02-28 21:21:46 +0700

    устанавливает redux

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {createStore} from "redux";
+import {Provider} from "react-redux";
+import {reducer} from "./reducer/reducer";
 import App from "./components/app/app";
 import questions from "./mocks/questions";
 // import Albums from "./components/albums/albums";
@@ -8,11 +11,15 @@ const Settings = {
   ERRORS_COUNT: 3
 };
 
+const store = createStore(reducer);
+
 ReactDOM.render(
-    <App
-      errorsCount={Settings.ERRORS_COUNT}
-      questions={questions}
-    />,
+    <Provider store={store}>
+      <App
+        errorsCount={Settings.ERRORS_COUNT}
+        questions={questions}
+      />
+    </Provider>,
 
     document.querySelector(`#root`)
 );
